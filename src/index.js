5d04048e6cbead9ccc537d53aed01facddec7488
Tysuiku <timothydong41@gmail.com>
2023-03-11 08:37:17 -0500

    css css css, still not positioned and toned for mobile view and start using script defer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import { displayAgentInfo } from "./scripts/agentDataFetch.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const search = document.querySelector("#search");
-  const agentInput = document.querySelector("#agentSearch");
-  const agentPortrait = document.querySelector("#agentBox");
-  const agentRole = document.querySelector("#agentRoleBox");
-  const agentAbility = document.querySelector("#agentAbilityBox");
+const search = document.querySelector("#search");
+const agentInput = document.querySelector("#agentSearch");
+const agentPortrait = document.querySelector("#agentBox");
+const agentRole = document.querySelector("#agentRoleBox");
+const agentAbility = document.querySelector("#agentAbilityBox");
 
-  search.addEventListener("click", function (e) {
-    const agentName = agentInput.value;
-    e.preventDefault();
-    displayAgentInfo(agentName);
-  });
+search.addEventListener("click", function (e) {
+  const agentName = agentInput.value;
+  e.preventDefault();
+  displayAgentInfo(agentName);
 });
