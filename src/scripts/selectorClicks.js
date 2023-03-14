54c51aaa5d0ce112ac55ffcc4e66f688b9911d9e
Tysuiku <timothydong41@gmail.com>
2023-03-14 15:19:52 -0400

    switch to flexbox, fix most stuff add sprites weapon stat and dropdown, next up search bar and dropdown style

--- src/scripts/selectorClicks.js
+++ src/scripts/selectorClicks.js
@@ -1,3 +1,5 @@
+import { displayWeaponInfo } from "./weaponDataFetch";
+
 export function selectorChange(weaponData, weaponName) {
   let weapon = null;
   for (let i = 0; i < weaponData.data.length; i++) {
@@ -9,26 +11,10 @@ export function selectorChange(weaponData, weaponName) {
     }
   }
 
-  //   const weaponSelector = document.querySelector("#weaponSelector");
-
-  //   weaponSelector.addEventListener("change", function () {
-  //     const selectedValue = weaponSelector.value;
-  //     weaponName = selectedValue;
-  //     console.log(selectedValue);
-  //     console.log(weaponName);
-  //     console.log(weapon);
-  //     console.log(weapon.displayIcon);
-  //   });
   const weaponSelector = document.querySelector("#weaponSelector");
 
   weaponSelector.addEventListener("change", (event) => {
     weaponName = event.target.value;
-    // weapon.displayName = event.target.value;
-    // document.querySelector("#weaponImg").src = weapon.displayIcon;
-    // console.log(selectedValue);
-
-    console.log(weaponName);
-    console.log(weapon);
-    console.log(weapon.displayIcon);
+    displayWeaponInfo(weaponName);
   });
 }
