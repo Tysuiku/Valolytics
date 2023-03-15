c20b7e444aa0df9f835aab6f5b0bf193daa51415
Tysuiku <timothydong41@gmail.com>
2023-03-15 00:57:17 -0400

    clean up css add alot of glow effects and gradient effects, scale effects

--- src/scripts/weaponDataFetch.js
+++ src/scripts/weaponDataFetch.js
@@ -1,16 +1,30 @@
 import { allWeaponQueries } from "./weaponQueries";
-import { selectorChange } from "./selectorClicks";
+
+let weaponData = null;
+const weaponSelector = document.querySelector("#weaponSelector");
+
+fetch(`https://valorant-api.com/v1/weapons`)
+  .then((response) => response.json())
+  .then((data) => {
+    weaponData = data;
+    weaponSelector.addEventListener("change", (event) => {
+      const weaponName = event.target.value;
+      displayWeaponInfo(weaponName);
+    });
+    const defaultWeaponName = "Vandal";
+    displayWeaponInfo(defaultWeaponName);
+  })
+  .catch((error) => console.error(error));
 
 export function displayWeaponInfo(weaponName) {
-  fetch(`https://valorant-api.com/v1/weapons`)
-    .then((response) => response.json())
-    .then((data) => {
-      let weaponData = data;
-      try {
-        allWeaponQueries(weaponData, weaponName);
-        selectorChange(weaponData, weaponName);
-      } catch (error) {
-        console.log("Weapon not found", error);
-      }
-    });
+  if (!weaponData) {
+    console.log("Weapon data not yet loaded.");
+    return;
+  }
+
+  try {
+    allWeaponQueries(weaponData, weaponName);
+  } catch (error) {
+    console.log("Weapon not found", error);
+  }
 }
